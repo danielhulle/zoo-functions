2700952cb5e2ea57245c1b9690c0610e03642d17
Daniel Hulle <danielhulle@gmail.com>
2023-03-09 19:42:31 -0300

    requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
+  const foundSpecies = data.species.filter(({ id }) => ids.includes(id));
 
+  return foundSpecies;
 };
 
 module.exports = getSpeciesByIds;
