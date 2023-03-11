7bc81f3880df72fa3386ab928cdee7ff49a652ea
Daniel Hulle <danielhulle@gmail.com>
2023-03-11 12:11:08 -0300

    requisito 4 feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -6,25 +6,20 @@ const isManager = (id) => {
   return findManager;
 };
 
-const checkManager = (managerId) => {
+const validateManager = (managerId) => {
   if (isManager(managerId) !== true) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 };
 
 const getRelatedEmployees = (managerId) => {
-  try {
-    checkManager(managerId);
-    const relatedEmployees = data.employees.filter(({ managers }) => managers.includes(managerId));
-    const employeesNames = relatedEmployees
-      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  validateManager(managerId);
 
-    return employeesNames;
-  } catch (error) {
-    return error.message;
-  }
+  const relatedEmployees = data.employees.filter(({ managers }) => managers.includes(managerId));
+  const employeesNames = relatedEmployees
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+
+  return employeesNames;
 };
-
-// console.log(getRelatedEmployees('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = { isManager, getRelatedEmployees };
