254a1f3ca38e0910434e26f264167e697046e8b2
Daniel Hulle <danielhulle@gmail.com>
2023-03-12 12:33:47 -0300

    requisito 11 feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,19 +1,56 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = (obj) => {
-  const findEmployee = data.employees.filter(({ firstName }) => obj.name === firstName);
-  const employeeCoverage = findEmployee.reduce((acc, curr) => {
+const allEmployees = (param) => {
+  const allEmployeesCoverage = [];
+  data.employees.reduce((acc, curr) => {
     let objAcc = acc;
     objAcc = {
       id: curr.id,
       fullName: `${curr.firstName} ${curr.lastName}`,
-      species: curr.responsibleFor,
+      species: curr.responsibleFor
+        .map((responsibleId) => data.species.find(({ id }) => id === responsibleId).name),
+      locations: curr.responsibleFor
+        .map((responsibleId) => data.species.find(({ id }) => id === responsibleId).location),
+    };
+    return allEmployeesCoverage.push(objAcc);
+  }, {});
+  return allEmployeesCoverage;
+};
+
+const targetEmployee = (target) => {
+  const employeeCoverage = target.reduce((acc, curr) => {
+    let objAcc = acc;
+    objAcc = {
+      id: curr.id,
+      fullName: `${curr.firstName} ${curr.lastName}`,
+      species: curr.responsibleFor
+        .map((responsibleId) => data.species.find(({ id }) => id === responsibleId).name),
+      locations: curr.responsibleFor
+        .map((responsibleId) => data.species.find(({ id }) => id === responsibleId).location),
     };
     return objAcc;
   }, {});
   return employeeCoverage;
 };
 
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+const verifyEmployee = (employee) => {
+  if (employee) {
+    const employeeInfo = data.employees.filter(({ firstName, lastName, id }) =>
+      employee.name === firstName || employee.name === lastName || employee.id === id);
+    if (employeeInfo.length === 0) {
+      throw new Error('Informações inválidas');
+    }
+  }
+};
+
+const getEmployeesCoverage = (employee) => {
+  verifyEmployee(employee);
+
+  if (!employee) return allEmployees();
+
+  const employeeInfo = data.employees.filter(({ firstName, lastName, id }) =>
+    employee.name === firstName || employee.name === lastName || employee.id === id);
+  return targetEmployee(employeeInfo);
+};
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,16 +1,37 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  const findAnimal = data.species.filter(({ name }) => scheduleTarget.includes(name));
-  const animalSchedule = findAnimal.reduce((acc, curr) => {
-    let arr = acc;
-    arr = curr.availability;
-    return arr;
-  }, {});
-
-  return animalSchedule;
+const getAllSchedules = (param) => {
+  const newObj = {};
+  Object.keys(data.hours).forEach((day) => {
+    if (day !== 'Monday') {
+      newObj[day] = {
+        officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+        exhibition: data.species
+          .filter(({ name, availability }) => availability.includes(day))
+          .map(({ name }) => name),
+      };
+    }
+    if (day === 'Monday') {
+      newObj[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+  return newObj;
 };
 
-console.log(getSchedule('lions'));
+const getSchedule = (scheduleTarget) => {
+  if (scheduleTarget) {
+    const findAnimal = data.species.filter(({ name }) => scheduleTarget.includes(name));
+    const animalSchedule = findAnimal.reduce((acc, curr) => {
+      let arr = acc;
+      arr = curr.availability;
+      return arr;
+    }, {});
+    return animalSchedule;
+  }
+  return getAllSchedules();
+};
 
 module.exports = getSchedule;
