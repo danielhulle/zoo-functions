ceaa0d776e1d6442cdea34801a23df2481c7fac7
Daniel Hulle <danielhulle@gmail.com>
2023-03-12 16:08:17 -0300

    requisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const getSpeciesId = data.employees
+    .filter(({ id: employeeId }) => employeeId === id)
+    .map(({ responsibleFor }) => responsibleFor[0]);
+
+  const getSpeciesInfo = data.species.find(({ id: animalId }) => getSpeciesId.includes(animalId));
+  const getAnimalsAge = getSpeciesInfo.residents.map(({ age }) => age).sort((a, b) => b - a);
+  const getOldestAnimal = getSpeciesInfo.residents.filter(({ age }) => getAnimalsAge[0] === age);
+
+  const oldestAnimalInfo = getOldestAnimal.reduce((acc, curr) => {
+    let values = acc;
+    values = Object.values(curr);
+    return values;
+  }, []);
+
+  return oldestAnimalInfo;
 };
 
 module.exports = getOldestFromFirstSpecies;
